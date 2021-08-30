9b3b812d87134f6cbf32420097078b73a104ab17
zzx1996 <zzx520sdy@gmail.com>
2021-08-30 12:19:01 +0800

    增加文章类型

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -4,50 +4,31 @@
     justify-center
     align-center
   >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <v-card
-        class="mx-auto"
-        max-width="400"
+    <v-container>
+      <v-row
+        justify="start"
       >
-        <v-img
-          class="white--text align-end"
-          height="200px"
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        <v-col
+          v-for="i in 5"
+          :key="i"
+          cols="3"
         >
-          <v-card-title>Top 10 Australian beaches</v-card-title>
-        </v-img>
+          <v-card
 
-        <v-card-subtitle class="pb-0">
-          Number 10
-        </v-card-subtitle>
-
-        <v-card-text class="text--primary">
-          <div>Whitehaven Beach</div>
-
-          <div>Whitsunday Island, Whitsunday Islands</div>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-btn
-            color="orange"
-            text
+            class="mx-auto"
+            max-width="400"
           >
-            Share
-          </v-btn>
-
-          <v-btn
-            color="orange"
-            text
-          >
-            Explore
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
+            <v-img
+              class="white--text align-end"
+              height="200px"
+              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            >
+              <v-card-title>Top 10 Australian beaches</v-card-title>
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-layout>
 </template>
 
